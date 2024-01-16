8ebfe74c6b1719c3420bbe350f16546e7bd76cfb
Flavia Taho <flavia.taho@proton.me>
2024-01-16 15:18:35 -0300

    feat: teste novo

--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ console.log("I'm a new breaking change feature!");
 console.log("I'm a new feature");
 console.log("I'm a new fix");
 console.log("I'm a new feature");
-console.log("I'm a new fix");
-console.log("I'm a new feature");
-console.log("I'm a new fix");
-console.log("I'm a new feature");
-console.log("I'm a new fix");
