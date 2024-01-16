4386b8115db1a2dc258ab2183cfe4ff540c760d3
Flávia Taho <flaviotaho@gmail.com>
2024-01-16 15:20:16 -0300

    Feat/2 (#209)
    * ci: test log
    
    * ci: test log
    
    * ci: change git.changelog command
    
    * ci: change git.changelog command
    
    * ci: change git.changelog command
    
    * feat: novo feature teste squash
    
    * fix: novo feature teste squash
    
    * feat: teste novo
    
    * fix: teste novo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ console.log("I'm a new feature");
 console.log("I'm a new fix");
 console.log("I'm a new feature");
 console.log("I'm a new fix");
-console.log("I'm a new feature");
-console.log("I'm a new fix");
-console.log("I'm a new feature");
-console.log("I'm a new fix");
